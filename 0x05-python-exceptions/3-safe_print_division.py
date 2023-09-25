634607f4cd96714c61f087673abdac44f6c09803
Henry <akinwalehenry9@gmail.com>
2023-09-25 13:44:51 +0100

    One fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div = 0
     try:
         div = a / b
-        return div
-    except:
+    except (ZeroDivisionError, FloatingPointError):
         div = None
-        return None
     finally:
         print("Inside result: {}".format(div))
+    return div
